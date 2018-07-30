411e943a4856c18d0a0aee8053c99d8a34221dbd
LaurenWorkosky <19lworkosky@student.dasd.org>
2018-07-30 14:23:27 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,11 +1,9 @@
 class AmazonTransaction
   def initialize (total, discount)
     @total = total
+    @total = [0]
     @discount = discount
     @items = []
   end
-  
-  def total
-    @total = 0
-  end
+
 end
